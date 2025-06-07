4d4ff6d4f51fd23976b85f67054bf8ef60e0a236
Corazon654 <serezha_kim_2001@mail.ru>
2025-06-07 18:32:10 +0300

    Add files via upload

---
+++ Privet.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp30
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            switch (DateTime.Now.DayOfWeek)
+            {
+
+                case DayOfWeek.Monday:
+
+                    Console.WriteLine("Kon'nichiwa");
+ 
+           break;
+
+                case DayOfWeek.Tuesday:
+
+                    Console.WriteLine("Annyeong");
+
+                    break;
+
+                case DayOfWeek.Wednesday:
+
+                    Console.WriteLine("Hola");
+
+                    break;
+
+                case DayOfWeek.Thursday:
+
+                    Console.WriteLine("Hallo");
+
+                    break;
+
+                case DayOfWeek.Friday:
+
+                    Console.WriteLine("Bonjour");
+
+                    break;
+
+                case DayOfWeek.Saturday:
+
+                    Console.WriteLine("Nǐ hǎo");
+
+                    break;
+
+                case DayOfWeek.Sunday:
+
+                    Console.WriteLine("Hej");
+
+                    break;
+            }
+            Console.ReadLine();
+
+            }
+    }
+}
